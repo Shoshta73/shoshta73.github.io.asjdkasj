529553733314aa08bf277dcf31df938567a88a38
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-02-12 22:00:02 +0100

    fix(frontend): add logic for displaying link in footer
    depends on VITE_APP_DEPLOYMENT env variable

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -9,7 +9,12 @@ const Footer = () => {
         <GithubIcon size={50} role="img" aria-label="github icon" />
       </div>
       <p>
-        &copy; 2024 <a href="https://shoshta.net">shoshta.net</a>
+        &copy; 2024{" "}
+        {import.meta.env.VITE_APP_DEPLOYMENT === "GHP" ? (
+          <a href="https://shoshta73.github.io">shoshta73.github.io</a>
+        ) : (
+          <a href="https://shoshta.net">shoshta.net</a>
+        )}
       </p>
     </div>
   );
